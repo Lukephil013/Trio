c1efe5d4993728bb26290a3be408a4d3ec00c5bb
Luke Philips <lukephil013@gmail.com>
2019-02-20 11:38:46 -0800

    Fixed descending order

--- main.cpp
+++ main.cpp
@@ -1,43 +1,40 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using std::cout;
 using std::cin;
 using std::endl;
+using std::vector;
+
 
 void sortDescending(int,int,int);
 void swap(int&,int&);
 
+//sorts list in descending order.
 int main()
 {
+  vector<int> nums;
   int numA, numB, numC;
   cout<<"Enter any three numbers: ";
   cin>>numA>>numB>>numC;
-
-  sortDescending(numA, numB, numC);
+  nums.push_back(numA);
+  nums.push_back(numB);
+  nums.push_back(numC);
+  //5, 8, 2
+  for(int i = 0; i < nums.size(); i++){
+	  for(int j = 0; j < nums.size(); j++){
+		  if(nums[i] > nums[j]){
+			  int temp = nums[i];
+			  nums[i] = nums[j];
+			  nums[j] = temp;
+		  }
+	  }
+  }
   
   cout<<"From greatest to least, they are: ";
-  cout<<numA<<","<<numB<<","<<numC<<endl;
+  for(int k = 0; k < nums.size(); k++){
+	  cout<<nums[k]<<",";
+  }
   return 0;
 }
 
-void sortDescending(int first, int second, int third)
-{
-  if( first < third )
-  {
-    swap(first,third);
-  }
-  if( first < second )
-  {
-    swap(first,second);
-  }
-  if( second < third )
-  {
-    swap(second,third);
-  }
-}
-
-void swap(int &first, int &second)
-{
-  int temp = first;
-  first = second;
-  second = temp;
-}
